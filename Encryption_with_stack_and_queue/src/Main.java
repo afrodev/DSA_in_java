9b02ea6746e2a89c5b8d7c432b5a508fe0aa99f6
afrodev <monzergmc1@gmail.com>
2025-01-17 20:07:51 +0100

    Hypothesis checkpoint

---
+++ Encryption_with_stack_and_queue/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    /*
+    Queue: MON
+    Stack: ZER
+
+    Hypothesis for Encryption without ROT13: RMEOZN
+    Check if correct when the function is made without ROT13
+    */
+
+    
+
+}
